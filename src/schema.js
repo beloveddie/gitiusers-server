e68d1284399b378501f6bbccc2835b0e993f6f59
Eddiebee <edbeeotudor@gmail.com>
2022-10-06 13:03:18 +0100

    Initial Schema build

--- src/schema.js
+++ src/schema.js
@@ -1,1 +1,23 @@
-// Schemas
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+  # Entry points to our Schema
+  type Query {
+    "Query to get an array of Users for our Home grid."
+    usersForHome: [User!]!
+  }
+
+  "A type representing a GitHub User details"
+  type User {
+    "A unique Identifier for a User"
+    id: ID!
+    "A name used as a User login detail"
+    login: String!
+    "An avatar for a User"
+    avatar_url: String!
+    "A brief description of a User"
+    description: String!
+  }
+`;
+
+module.exports = typeDefs;
