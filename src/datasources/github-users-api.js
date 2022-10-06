f8a63cb2eaa234f9d10d050ff1c437efb3eb5c5c
Eddiebee <edbeeotudor@gmail.com>
2022-10-06 13:19:17 +0100

    Initial setup of GitHubUsersAPI  REST datasource

--- src/datasources/github-users-api.js
+++ src/datasources/github-users-api.js
@@ -1,1 +1,15 @@
-// GitHub users API
+const { RESTDataSource } = require("apollo-datasource-rest");
+
+class GitHubUsersAPI extends RESTDataSource {
+  constructor() {
+    super();
+    // this is the base URL for the GitHub API
+    this.baseURL = "https://api.github.com/";
+  }
+
+  getUsersForHome() {
+    return this.get("users");
+  }
+}
+
+module.exports = GitHubUsersAPI;
