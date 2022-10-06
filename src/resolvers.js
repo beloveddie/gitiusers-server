afe1fedaac86727336b32f5121602cb77391732d
Eddiebee <edbeeotudor@gmail.com>
2022-10-06 14:04:29 +0100

    Use dummy-text package to populate description

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,10 +1,12 @@
+const { generateSentence } = require("dummy-text-generator");
+
 const resolvers = {
   Query: {
     usersForHome: async (_, __, { dataSources }) => {
       const users = await dataSources.gitHubUsersAPI.getUsersForHome();
       const usersWithDescription = users.map((user) => ({
         ...user,
-        description: "Hello world!",
+        description: generateSentence(10),
       }));
       return usersWithDescription;
     },
