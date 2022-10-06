7a1b334f5bce43b0691f799d1c33e74b7165bd07
Eddiebee <edbeeotudor@gmail.com>
2022-10-06 13:48:17 +0100

    Initial resolvers build up

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,1 +1,14 @@
-// Resolvers
+const resolvers = {
+  Query: {
+    usersForHome: async (_, __, { dataSources }) => {
+      const users = await dataSources.gitHubUsersAPI.getUsersForHome();
+      const usersWithDescription = users.map((user) => ({
+        ...user,
+        description: "Hello world!",
+      }));
+      return usersWithDescription;
+    },
+  },
+};
+
+module.exports = resolvers;
