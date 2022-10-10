1fb388f78c743ee118ac70b1d67967351c899de5
Eddiebee <edbeeotudor@gmail.com>
2022-10-10 09:07:31 +0100

    Add comment on the usersForHome resolver

--- src/resolvers.js
+++ src/resolvers.js
@@ -2,6 +2,7 @@ const { generateSentence } = require("dummy-text-generator");
 
 const resolvers = {
   Query: {
+    // returns an array of Users that will used to populate the Homepage of the web client
     usersForHome: async (_, __, { dataSources }) => {
       const users = await dataSources.gitHubUsersAPI.getUsersForHome();
       const usersWithDescription = users.map((user) => ({
